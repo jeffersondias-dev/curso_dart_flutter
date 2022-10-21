bdc36a0198af6481363255696517d53d725b9b69
Jefferson Dias <96580079+jeffersondias-dev@users.noreply.github.com>
2022-10-21 15:55:17 -0300

    Create a Widget AppBar

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+      ),
+    );
+  }
+}
