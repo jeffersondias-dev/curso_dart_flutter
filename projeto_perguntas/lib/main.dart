32d1a6f2afd4e11c268beb29738556a03b4c9bac
Jefferson Dias <96580079+jeffersondias-dev@users.noreply.github.com>
2022-10-21 20:13:37 -0300

    create PerguntasApp StatefulWidget

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -4,12 +4,48 @@ main() {
   runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaApp extends StatefulWidget {
+  @override
+  State<PerguntaApp> createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      "Qual é a sua cor Favorita?",
+      "Qual é o seu animal Favorito?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[_perguntaSelecionada]),
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
         ),
       ),
     );
