111f238d61a097db2a14e772702e2d933534ef73
Jefferson Dias <96580079+jeffersondias-dev@users.noreply.github.com>
2022-10-22 08:20:18 -0300

    Refactor the code whith a map

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -37,12 +37,11 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
-    List<Widget> respostas = [];
+    List<String> respostas =
+        perguntas[_perguntaSelecionada].cast()['respostas'];
 
-    for (String textoResposta
-        in perguntas[_perguntaSelecionada].cast()['respostas']) {
-      respostas.add(Resposta(textoResposta, _responder));
-    }
+    List<Widget> widgets =
+        respostas.map((element) => Resposta(element, _responder)).toList();
 
     return MaterialApp(
       home: Scaffold(
@@ -52,7 +51,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            ...respostas,
+            ...widgets,
           ],
         ),
       ),
