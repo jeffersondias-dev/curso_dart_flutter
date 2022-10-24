a7c84773260c31e12f77ffcf54a41660015f104f
Jefferson Dias <96580079+jeffersondias-dev@users.noreply.github.com>
2022-10-24 08:37:38 -0300

    create class Questionario

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projeto_perguntas/questao.dart';
-import 'package:projeto_perguntas/respostas.dart';
+import 'package:projeto_perguntas/questionario.dart';
 import 'package:projeto_perguntas/resultado.dart';
 
 main() {
@@ -30,6 +29,10 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   var _perguntaSelecionada = 0;
 
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   /// _responder() is a function that is called when the user clicks on the button. It checks if there is
   /// a question selected, and if there is, it increments the _perguntaSelecionada variable by 1
   void _responder() {
@@ -42,36 +45,19 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   /// A getter that returns true if the _perguntaSelecionada is less than the length of the _perguntas
   /// list.
-  bool get temPerguntaSelecionada {
-    return _perguntaSelecionada < _perguntas.length;
-  }
 
   @override
   Widget build(BuildContext context) {
-    /// A ternary operator that checks if there is a question selected, and if there is, it returns the
-    /// list of answers of the question selected, otherwise it returns the list of answers of the first
-    /// question.
-    List<String> respostas = temPerguntaSelecionada
-        ? _perguntas[_perguntaSelecionada].cast()['respostas']
-        : _perguntas[0].cast()['respostas'];
-
-    /// Creating a list of widgets, where each widget is a Resposta widget, and each Resposta widget has
-    /// a text and a function.
-    List<Widget> widgets =
-        respostas.map((element) => Resposta(element, _responder)).toList();
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
         body: temPerguntaSelecionada
-            ? Column(
-                children: [
-                  Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
-                  ...widgets,
-                ],
-              )
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder)
             : const Resultado('Parabéns Jovem'),
       ),
     );
